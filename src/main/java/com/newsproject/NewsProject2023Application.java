85b8d1727c4d46dd865e14a6f61f41ba8acc6619
TuyenKC <khuctuyen2001@gmail.com>
2023-09-19 22:15:20 +0700

    init project

---
+++ src/main/java/com/newsproject/NewsProject2023Application.java
@@ -0,0 +1,13 @@
+package com.newsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsProject2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsProject2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/newsproject/NewsProject2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsProject2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
